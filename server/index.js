725fc53f492fb6ef5359d7f17011b97208f7fa7b
Carson <carsonweinand18@gmail.com>
2021-05-24 12:52:41 -0400

    got proxy working

--- server/index.js
+++ server/index.js
@@ -9,10 +9,11 @@ const path = require("path");
 const ProxyPort = 2000;
 
 const Photos = "http://localhost:2004";
-const Description = "http://localhost:5001";
+const HostedBy = "http://localhost:5003";
 const CalendarWidget = "http://localhost:2002";
 
 app.use(express.static("client/dist"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
